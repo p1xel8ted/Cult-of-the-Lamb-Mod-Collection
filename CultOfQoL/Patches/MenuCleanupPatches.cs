59e049d3344eecf0e1ce8d289c4e8ace56e1617b
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-05-01 18:09:32 +1000

    Remove clutter from main and pause menus, ads and buttons.

--- CultOfQoL/Patches/MenuCleanupPatches.cs
+++ CultOfQoL/Patches/MenuCleanupPatches.cs
@@ -1,6 +1,8 @@
-﻿using HarmonyLib;
+﻿using System.ComponentModel;
+using HarmonyLib;
 using Lamb.UI.MainMenu;
 using Lamb.UI.PauseMenu;
+using UnityEngine;
 
 namespace CultOfQoL.Patches;
 
@@ -14,14 +16,56 @@ public static class MenuCleanupPatches
         if (!Plugin.RemoveMenuClutter.Value) return;
         __instance._creditsButton.gameObject.SetActive(false);
         __instance._roadmapButton.gameObject.SetActive(false);
+       
     }
     
     [HarmonyPostfix]
     [HarmonyPatch(typeof(UIMainMenuController), nameof(UIMainMenuController.Awake))]
     public static void UIMainMenuController_Awake(ref UIMainMenuController __instance)
     {
-        if (!Plugin.RemoveMenuClutter.Value) return;
-        __instance._discordButton.gameObject.SetActive(false);
+        Plugin.UIMainMenuController = __instance;
+        if (Plugin.RemoveMenuClutter.Value)
+        {
+            __instance._discordButton.gameObject.SetActive(false);
+            var stuff = GameObject.Find("Main Menu Controller/Main Menu/MainMenuContainer/Right/Transform/");
+            if (stuff != null)
+            {
+                stuff.gameObject.SetActive(false);
+            }
+        }
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(UIPauseMenuController), nameof(UIPauseMenuController.Awake))]
+    public static void UIPauseMenuController_Awake(ref UIPauseMenuController __instance)
+    {
+        if (Plugin.RemoveHelpButtonInPauseMenu.Value)
+        {
+            __instance._helpButton.gameObject.SetActive(false);
+        }
+        if (Plugin.RemovePhotoModeButtonInPauseMenu.Value)
+        {
+            __instance._photoModeButton.gameObject.SetActive(false);
+        }
+        if (Plugin.RemoveTwitchButtonInPauseMenu.Value)
+        {
+            __instance._twitchSettingsButton.gameObject.SetActive(false);
+        }
+    }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(MenuAdController), nameof(MenuAdController.Start))]
+    //[HarmonyPatch(typeof(MenuAdController), nameof(MenuAdController.Update))]
+    public static void MenuAdController_Start(ref MenuAdController __instance)
+    {
+        var one = GameObject.Find("Main Menu Controller/Main Menu/MainMenuContainer/Right/Ad/Outline");
+        var two = GameObject.Find("Main Menu Controller/Main Menu/MainMenuContainer/Right/Ad/Transform");
+        one.SetActive(false);
+        two.SetActive(false);
+        // __instance.ads.Clear();
+        // __instance.nonRemoteAds.Clear();
+        // __instance._bannerObj = null;
+        __instance.gameObject.SetActive(false);
     }
 
     [HarmonyPostfix]
