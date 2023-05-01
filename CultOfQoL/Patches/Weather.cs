8d276645d3d13f89c92fc3ca11b2c5dacc3057b1
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-05-01 18:06:56 +1000

    Refactor Weather.cs to allow for changing weather on phase change and random weather change when exiting area.

--- CultOfQoL/Patches/Weather.cs
+++ CultOfQoL/Patches/Weather.cs
@@ -1,155 +1,86 @@
-// using System;
-// using System.Collections.Generic;
-// using System.Linq;
-// using System.Reflection.Emit;
-// using HarmonyLib;
-// using UnityEngine;
-//
-// namespace CultOfQoL.Patches;
-//
-// [HarmonyPatch]
-// public static class Weather
-// {
-//     [HarmonyPostfix]
-//     [HarmonyPatch(typeof(TimeManager), nameof(TimeManager.StartNewPhase))]
-//     public static void TimeManager_StartNewPhase(TimeManager __instance, ref DayPhase phase)
-//     {
-//         if (Plugin.MoreDynamicWeather.Value)
-//         {
-//             if (Plugin.ChangeWeatherOnPhaseChange.Value)
-//             {
-//                 WeatherController.Instance.CheckWeather();
-//                 Plugin.L($"New phase: {phase.ToString()}, changing weather!");
-//             }
-//         }
-//
-//         if (Plugin.ShowPhaseNotifications.Value && phase is not DayPhase.Count or DayPhase.None)
-//         {
-//             NotificationCentre.Instance.PlayGenericNotification($"{phase.ToString()} has started!");
-//         }
-//     }
-//
-//
-//     /*[HarmonyTranspiler]
-//     [HarmonyPatch(typeof(BiomeBaseManager), nameof(BiomeBaseManager.ActivateRoom))]
-//     [HarmonyPatch(typeof(RoomSwapManager), nameof(RoomSwapManager.ActivateRoom))]
-//     public static IEnumerable<CodeInstruction> ActivateRoom_Transpiler(IEnumerable<CodeInstruction> instructions)
-//     {
-//         var instructionList = instructions.ToList();
-//         if (!Plugin.MoreDynamicWeather.Value) return instructionList.AsEnumerable();
-//         for (var i = 0; i < instructionList.Count; i++)
-//         {
-//             if (instructionList[i].Calls(typeof(WeatherController).GetMethod(nameof(WeatherController.CheckWeather))))
-//             {
-//                 instructionList.RemoveRange(i - 1, 2);
-//             }
-//         }
-//
-//         return instructionList.AsEnumerable();
-//     }*/
-//
-//
-//     // [HarmonyPatch(typeof(WeatherController), nameof(WeatherController.CheckWeather))]
-//     // public static class WeatherControllerCheckWeather
-//     // {
-//     //     [HarmonyPrefix]
-//     //     public static void Prefix(ref WeatherController __instance)
-//     //     {
-//     //         if (!Plugin.MoreDynamicWeather.Value) return;
-//     //         __instance.chanceOfRain = 100;
-//     //         __instance.chanceOfWind = 100;
-//     //         WeatherController.InWeatherOverride = false;
-//     //     }
-//     //
-//     //     [HarmonyTranspiler]
-//     //     public static IEnumerable<CodeInstruction> CheckWeather_Transpiler(IEnumerable<CodeInstruction> instructions)
-//     //     {
-//     //         var instructionList = instructions.ToList();
-//     //         if (!Plugin.MoreDynamicWeather.Value) return instructionList.AsEnumerable();
-//     //         var countSixteen = 0;
-//     //         var countSeventeen = 0;
-//     //
-//     //         for (var i = 0; i < instructionList.Count; i++)
-//     //         {
-//     //             if (instructionList[i].opcode != OpCodes.Ldc_I4_S) continue;
-//     //             if (instructionList[i].operand is sbyte value)
-//     //             {
-//     //                 switch (value)
-//     //                 {
-//     //                     case 16:
-//     //                         countSixteen++;
-//     //                         instructionList[i].operand = countSixteen switch
-//     //                         {
-//     //                             1 => Mathf.Abs(Plugin.RainLowerChance.Value),
-//     //                             2 => Mathf.Abs(Plugin.WindLowerChance.Value),
-//     //                             _ => instructionList[i].operand
-//     //                         };
-//     //                         instructionList[i + 1].opcode = OpCodes.Bgt_S;
-//     //                         break;
-//     //                     case 17:
-//     //                         countSeventeen++;
-//     //                         instructionList[i].operand = countSeventeen switch
-//     //                         {
-//     //                             1 => Mathf.Abs(Plugin.RainUpperChance.Value),
-//     //                             2 => Mathf.Abs(Plugin.WindUpperChance.Value),
-//     //                             _ => instructionList[i].operand
-//     //                         };
-//     //                         instructionList[i + 1].opcode = OpCodes.Blt_S;
-//     //                         break;
-//     //                 }
-//     //             }
-//     //         }
-//     //
-//     //         return instructionList.AsEnumerable();
-//     //     }
-//     //
-//     //     [HarmonyPostfix]
-//     //     public static void Postfix(ref WeatherController __instance)
-//     //     {
-//     //         if (!Plugin.ShowWeatherChangeNotifications.Value) return;
-//     //         if (WeatherController.insideBuilding) return;
-//     //
-//     //         var lightRain = Math.Abs(__instance.weatherManager.RainIntensity - 0.15f) < 0.01f && WeatherController.isRaining && WeatherController.IsActive;
-//     //         var heavyRain = Math.Abs(__instance.weatherManager.RainIntensity - 0.25f) < 0.01f && WeatherController.isRaining && WeatherController.IsActive;
-//     //         var lightWind = Math.Abs(__instance.weatherManager.windDensity - 0.15f) < 0.01f && WeatherController.IsActive;
-//     //         var heavyWind = Math.Abs(__instance.weatherManager.windDensity - 0.25f) < 0.01f && WeatherController.IsActive;
-//     //
-//     //         if (heavyRain && heavyWind)
-//     //         {
-//     //             NotificationCentre.Instance.PlayGenericNotification("Heavy rain and wind!");
-//     //         }
-//     //         else if (lightRain && lightWind)
-//     //         {
-//     //             NotificationCentre.Instance.PlayGenericNotification("Light rain and wind!");
-//     //         }
-//     //         else if (lightRain && heavyWind)
-//     //         {
-//     //             NotificationCentre.Instance.PlayGenericNotification("Light rain and heavy wind!");
-//     //         }
-//     //         else if (heavyRain && lightWind)
-//     //         {
-//     //             NotificationCentre.Instance.PlayGenericNotification("Heavy rain and light wind!");
-//     //         }
-//     //         else if (heavyRain)
-//     //         {
-//     //             NotificationCentre.Instance.PlayGenericNotification("Heavy rain!");
-//     //         }
-//     //         else if (heavyWind)
-//     //         {
-//     //             NotificationCentre.Instance.PlayGenericNotification("Heavy wind!");
-//     //         }
-//     //         else if (lightRain)
-//     //         {
-//     //             NotificationCentre.Instance.PlayGenericNotification("Light rain!");
-//     //         }
-//     //         else if (lightWind)
-//     //         {
-//     //             NotificationCentre.Instance.PlayGenericNotification("Light wind!");
-//     //         }
-//     //         else
-//     //         {
-//     //             NotificationCentre.Instance.PlayGenericNotification("Clear skies!");
-//     //         }
-//     //     }
-//     // }
-// }
+using HarmonyLib;
+using Map;
+
+namespace CultOfQoL.Patches;
+
+[HarmonyPatch]
+public static class Weather
+{
+    private static WeatherSystemController? WeatherSystemController { get; set; }
+    
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(TimeManager), nameof(TimeManager.StartNewPhase))]
+    public static void TimeManager_StartNewPhase(TimeManager __instance, ref DayPhase phase)
+    {
+        if (Plugin.ChangeWeatherOnPhaseChange.Value)
+        {
+            if (WeatherSystemController != null)
+            {
+                var weather = WeatherSystemController.weatherData.RandomElement();
+                WeatherSystemController.SetWeather(weather.WeatherType, weather.WeatherStrength, 3f);
+            }
+
+            Plugin.L($"New phase: {phase.ToString()}, changing weather!");
+        }
+
+
+        if (Plugin.ShowPhaseNotifications.Value && phase is not DayPhase.Count or DayPhase.None)
+        {
+            NotificationCentre.Instance.PlayGenericNotification($"{phase.ToString()} has started!");
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(WeatherSystemController), nameof(WeatherSystemController.Awake))]
+    [HarmonyPatch(typeof(WeatherSystemController), nameof(WeatherSystemController.Start))]
+    public static void WeatherSystemController_Assign(ref WeatherSystemController? __instance)
+    {
+        WeatherSystemController = __instance;  
+    }
+    
+    [HarmonyPrefix]
+    [HarmonyPatch(typeof(WeatherSystemController), nameof(WeatherSystemController.SetWeather))]
+    public static void WeatherSystemController_SetWeather_Prefix(ref WeatherSystemController __instance, ref WeatherSystemController.WeatherType weatherType,
+        ref WeatherSystemController.WeatherStrength weatherStrength)
+    {
+        if (Plugin.RandomWeatherChangeWhenExitingArea.Value)
+        {
+            if (WeatherSystemController != null)
+            {
+                var weather = WeatherSystemController.weatherData.Random();
+                weatherType = weather.WeatherType;
+                weatherStrength = weather.WeatherStrength;
+            }
+        }
+    }
+
+    [HarmonyPostfix]
+    [HarmonyPatch(typeof(WeatherSystemController), nameof(WeatherSystemController.SetWeather))]
+    public static void WeatherSystemController_SetWeather_Postfix(ref WeatherSystemController.WeatherType weatherType,
+        WeatherSystemController.WeatherStrength weatherStrength, float transitionDuration)
+    {
+        if (!Plugin.ShowWeatherChangeNotifications.Value) return;
+        
+        var ws = string.Empty;
+        switch (weatherType)
+        {
+            case WeatherSystemController.WeatherType.None:
+                break;
+            case WeatherSystemController.WeatherType.Raining:
+                ws = "Rain";
+                break;
+            case WeatherSystemController.WeatherType.Windy:
+                ws = "Wind";
+                break;
+            case WeatherSystemController.WeatherType.Snowing:
+                ws = "Snow";
+                break;
+            default:
+                return;
+        }
+
+        NotificationCentre.Instance.PlayGenericNotification(weatherType == WeatherSystemController.WeatherType.None
+            ? "Weather cleared!"
+            : $"Weather changed to {weatherStrength.ToString()} {ws}!");
+    }
+}
