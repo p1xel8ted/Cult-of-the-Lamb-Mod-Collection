06e0f45b1d46d63c23944ccdcd8e821315c8790c
p1xel8ted <10510767+p1xel8ted@users.noreply.github.com>
2023-05-02 18:47:16 +1000

    Add UnityEngine using statement and update Update method in UnityEvents.cs

--- CultOfQoL/UnityEvents.cs
+++ CultOfQoL/UnityEvents.cs
@@ -1,4 +1,5 @@
 using System;
+using UnityEngine;
 
 namespace CultOfQoL;
 
@@ -6,6 +7,12 @@ public partial class Plugin
 {
     private void Update()
     {
+        if (Plugin.EnableQuickSaveShortcut.Value && Plugin.SaveKeyboardShortcut.Value.IsUp())
+        {
+            SaveAndLoad.Save();
+            NotificationCentre.Instance.PlayGenericNotification("Game Saved!");
+        }
+        
         if (Plugin.DisableAd.Value && UIMainMenuController!=null)
         {
             foreach (var comp in UIMainMenuController.ad.GetComponents<UnityEngine.Component>())
